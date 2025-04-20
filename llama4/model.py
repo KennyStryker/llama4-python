0b1f2afed486a7d8f04ab09ed4161a373ad4f884
Kenny Stryker <nggkenny@gmail.com>
2025-04-20 12:06:36 +0000

    Fix config

--- llama4/model.py
+++ llama4/model.py
@@ -7,4 +7,5 @@ class Llama4(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def forward(self, inp: torch.Tensor): ...
+    def forward(self, inp: torch.Tensor):
+        return inp
