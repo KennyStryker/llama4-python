f128d09908645465b64fdd80d6e772e4e65730e5
Kenny Stryker <nggkenny@gmail.com>
2025-04-20 11:58:21 +0000

    Class init

---
+++ llama4/model.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+
+class Llama4(nn.Module):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, inp: torch.Tensor): ...
